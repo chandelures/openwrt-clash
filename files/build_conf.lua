2628c5a60ee9c679ddd31779b59792c39caf3566
chandelures <chandelurewang@gmail.com>
2021-10-17 00:03:11 +0800

    refactor: build configuration script

--- files/build_conf.lua
+++ files/build_conf.lua
@@ -9,11 +9,13 @@ local yamlext = ".yaml"
 local profile_dir = ucursor:get(config, "global", "profile_dir")
 local current_profile = ucursor:get(config, "global", "current_profile")
 
+local profile = {}
+
 local function profile_path()
     return profile_dir .. "/" .. current_profile .. yamlext
 end
 
-local function fetch_profile()
+local function fetch()
     local file, err = io.open(profile_path(), "r")
     if err == nil then
         return 0
@@ -24,17 +26,15 @@ local function fetch_profile()
     file:close()
 end
 
-local function load_profile()
-    local profile = {}
+local function load_origin()
     local file, err = io.open(profile_path(), "r")
     if err == nil then
         profile = lyaml.load(file:read("*a"), {all = true})[1]
         file:close()
     end
-    return profile
 end
 
-local function general(profile)
+local function general()
     local tproxy_enabled = ucursor:get_bool(config, "global", "tproxy_enabled")
     local api_host = ucursor:get(config, "global", "api_host")
     local api_port = ucursor:get(config, "global", "api_port")
@@ -51,7 +51,7 @@ local function general(profile)
     profile["ipv6"] = false
 end
 
-local function dns(profile)
+local function dns()
     local dns_host = ucursor:get(config, "dns", "host")
     local dns_port = ucursor:get(config, "dns", "port")
 
@@ -65,7 +65,7 @@ local function dns(profile)
     profile["dns"] = profile_dns
 end
 
-local function build(profile)
+local function build()
     local confdir = ucursor:get(config, "global", "confdir")
     local confpath = confdir .. "/config.yaml"
     local file = io.open(confpath, "w")
@@ -74,11 +74,11 @@ local function build(profile)
 end
 
 local function main()
-    fetch_profile()
-    local profile = load_profile()
-    general(profile)
-    dns(profile)
-    build(profile)
+    fetch()
+    load_origin()
+    general()
+    dns()
+    build()
 end
 
 main()
