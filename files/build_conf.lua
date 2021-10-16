824d935eba6d45d84d13df0cddc0b3f4a2edc471
Chandelure Wang <chandelurewang@gmail.com>
2021-10-17 00:08:36 +0800

    feat: add custom profile support (#28)

---
+++ files/build_conf.lua
@@ -0,0 +1,84 @@
+#!/usr/bin/lua
+
+local lyaml = require "lyaml"
+local ucursor = require "luci.model.uci"
+
+local config = "clash"
+local yamlext = ".yaml"
+
+local profile_dir = ucursor:get(config, "global", "profile_dir")
+local current_profile = ucursor:get(config, "global", "current_profile")
+
+local profile = {}
+
+local function profile_path()
+    return profile_dir .. "/" .. current_profile .. yamlext
+end
+
+local function fetch()
+    local file, err = io.open(profile_path(), "r")
+    if err == nil then
+        return 0
+    end
+
+    file = io.open(profile_path(), "w")
+    file:write("mode: direct")
+    file:close()
+end
+
+local function load_origin()
+    local file, err = io.open(profile_path(), "r")
+    if err == nil then
+        profile = lyaml.load(file:read("*a"), {all = true})[1]
+        file:close()
+    end
+end
+
+local function general()
+    local tproxy_enabled = ucursor:get_bool(config, "global", "tproxy_enabled")
+    local api_host = ucursor:get(config, "global", "api_host")
+    local api_port = ucursor:get(config, "global", "api_port")
+
+    if tproxy_enabled then
+        local tproxy_port = ucursor:get(config, "global", "tproxy_port")
+        profile["tproxy-port"] = tonumber(tproxy_port)
+    else
+        profile["tproxy-port"] = nil
+    end
+
+    profile["redir-port"] = nil
+    profile["external-controller"] = api_host .. ":" .. api_port
+    profile["ipv6"] = false
+end
+
+local function dns()
+    local dns_host = ucursor:get(config, "dns", "host")
+    local dns_port = ucursor:get(config, "dns", "port")
+
+    local profile_dns = {}
+    profile_dns["enable"] = true
+    profile_dns["ipv6"] = false
+    profile_dns["enhanced-mode"] = "redir-host"
+    profile_dns["listen"] = dns_host .. ":" .. dns_port
+    profile_dns["nameserver"] = {"114.114.114.114"}
+    profile_dns["fallback"] = {"https://1.1.1.1/dns-query"}
+    profile["dns"] = profile_dns
+end
+
+local function build()
+    local confdir = ucursor:get(config, "global", "confdir")
+    local confpath = confdir .. "/config.yaml"
+    local file = io.open(confpath, "w")
+    file:write(lyaml.dump({profile}))
+    file:close()
+end
+
+local function main()
+    fetch()
+    load_origin()
+    general()
+    dns()
+    build()
+end
+
+main()
