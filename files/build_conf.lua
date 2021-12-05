0a66bb6a1b4ac5f2481a21b154e37951e1103e13
Chandelure Wang <chandelurewang@gmail.com>
2021-12-05 23:44:28 +0800

    refactor: build clash configuration (#56)

--- files/build_conf.lua
+++ files/build_conf.lua
@@ -9,10 +9,7 @@ local config = "clash"
 local yamlext = ".yaml"
 local confdir = "/etc/clash"
 local profile_dir = "/etc/clash/profiles"
-
 local current_profile = x:get(config, "global", "current_profile")
-
-local profile = {}
 
 local function path_exist(path)
     local file, err = io.open(path, "r")
@@ -21,6 +18,23 @@ local function path_exist(path)
         return true
     end
     return false
+end
+
+local function get(section, option)
+    return x:get(config, section, option)
+end
+
+local function get_bool(section, option)
+    value = x:get(config, section, option)
+    return value == "1" or value == "true"
+end
+
+local function get_number(section, option)
+    value = x:get(config, section, option)
+    if tonumber(value) == 0 then
+        return nil
+    end
+    return tonumber(value)
 end
 
 local function profile_path()
@@ -41,53 +55,36 @@ local function fetch()
     file:close()
 end
 
-local function load_origin()
+local function load()
     local file = io.open(profile_path(), "r")
     profile = lyaml.load(file:read("*a"), {all = true})[1]
     file:close()
+    return profile
 end
 
-local function general()
-    local http_port = x:get(config, "global", "http_port")
-    local socks_port = x:get(config, "global", "socks_port")
-    local mixed_port = x:get(config, "global", "mixed_port")
-    local tproxy_enabled = x:get(config, "global", "tproxy_enabled")
-    local tproxy_port = x:get(config, "global", "tproxy_port")
-    local allow_lan = x:get(config, "global", "allow_lan")
-    local bind_addr = x:get(config, "global", "bind_addr")
-    local mode = x:get(config, "global", "mode")
-    local log_level = x:get(config, "global", "log_level")
-    local api_host = x:get(config, "global", "api_host")
-    local api_port = x:get(config, "global", "api_port")
-    local external_ui = x:get(config, "global", "external_ui")
+local function update_general(profile)
+    local http_port = get_number("global", "http_port")
+    local socks_port = get_number("global", "socks_port")
+    local mixed_port = get_number("global", "mixed_port")
+    local tproxy_enabled = get_bool("global", "tproxy_enabled")
+    local tproxy_port = get_number("global", "tproxy_port")
+    local allow_lan = get_bool(config, "global", "allow_lan")
+    local bind_addr = get("global", "bind_addr")
+    local mode = get("global", "mode")
+    local log_level = get("global", "log_level")
+    local api_host = get("global", "api_host")
+    local api_port = get_number("global", "api_port")
 
-    if tonumber(http_port) ~= 0 then
-        profile["port"] = tonumber(http_port)
-    else
-        profile["port"] = nil
-    end
-
-    if tonumber(socks_port) ~= 0 then
-        profile["socks-port"] = tonumber(socks_port)
-    else
-        profile["socks-port"] = nil
-    end
-
-    if tonumber(mixed_port) ~= 0 then
-        profile["mixed-port"] = tonumber(mixed_port)
-    else
-        profile["mixed-port"] = nil
-    end
-
-    if tonumber(tproxy_enabled) == 1 then
-        profile["tproxy-port"] = tonumber(tproxy_port)
+    profile["http-port"] = http_port
+    profile["socks-port"] = socks_port
+    profile["mixed-port"] = mixed_port
+    if tproxy_enabled then
+        profile["tproxy-port"] = tproxy_port
     else
         profile["tproxy-port"] = nil
     end
 
-    profile["redir-port"] = nil
-
-    if tonumber(allow_lan) == 1 then
+    if allow_lan then
         profile["allow-lan"] = true
         profile["bind-address"] = bind_addr
     else
@@ -97,24 +94,15 @@ local function general()
 
     profile["mode"] = mode
     profile["log-level"] = log_level
-    profile["ipv6"] = false
     profile["external-controller"] = api_host .. ":" .. api_port
-
-    if path_exist(confdir .. "/" .. external_ui) then
-        profile["external-ui"] = external_ui
-    else
-        profile["external-ui"] = nil
-    end
-
-    profile["interface-name"] = nil
 end
 
-local function dns()
-    local dns_host = x:get(config, "global", "dns_host")
-    local dns_port = x:get(config, "global", "dns_port")
-    local default_nameserver = x:get(config, "global", "default_nameserver")
-    local nameserver = x:get(config, "global", "nameserver")
-    local fallback = x:get(config, "global", "fallback")
+local function update_dns(profile)
+    local dns_host = get("global", "dns_host")
+    local dns_port = get_number("global", "dns_port")
+    local default_nameserver = get("global", "default_nameserver")
+    local nameserver = get("global", "nameserver")
+    local fallback = get("global", "fallback")
 
     local profile_dns = {}
     profile_dns["enable"] = true
@@ -124,22 +112,30 @@ local function dns()
     profile_dns["default-nameserver"] = default_nameserver
     profile_dns["nameserver"] = nameserver
     profile_dns["fallback"] = fallback
+
     profile["dns"] = profile_dns
 end
 
-local function build()
-    local confpath = confdir .. "/config.yaml"
-    local file = io.open(confpath, "w")
+local function drop_useless(profile)
+    profile["redir-port"] = nil
+    profile["external-ui"] = nil
+    profile["profile"] = nil
+    profile["ipv6"] = false
+end
+
+local function dump(profile)
+    local file = io.open(confdir .. "/config.yaml", "w")
     file:write(lyaml.dump({profile}))
     file:close()
 end
 
 local function main()
     fetch()
-    load_origin()
-    general()
-    dns()
-    build()
+    profile = load()
+    update_general(profile)
+    update_dns(profile)
+    drop_useless(profile)
+    dump(profile)
 end
 
 main()
